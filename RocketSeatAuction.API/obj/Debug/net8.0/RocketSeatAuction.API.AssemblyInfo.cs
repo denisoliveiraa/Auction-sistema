024f03c03fa76fd629a6cc968a2a83bf92e07309
denisoliveiraa <denis_o@hotmail.com.br>
2025-02-27 00:12:33 -0300

    Adding feature for test Auction

--- RocketSeatAuction.API/obj/Debug/net8.0/RocketSeatAuction.API.AssemblyInfo.cs
+++ RocketSeatAuction.API/obj/Debug/net8.0/RocketSeatAuction.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketSeatAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ab3c00be938272d34c00430d4525efc8061ff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e82965eaefe49d4aaa734040513e2f692cbd7a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RocketSeatAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RocketSeatAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Test/UseCases.Test/Auction/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Moq;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+namespace UseCases.Test.Auction.GetCurrent;
+public  class GetCurrentAuctionUseCaseTest
+{
+    [Fact]  
+    public void Sucess()
+    {
+
+        var mock = new Mock<IAuctionRepository>
+
+        var useCase = new GetCurrentAuctionUseCase(null);
+
+        var auction = useCase.Execute();
+
+
+        auction.Should().NotBeNull();
+    }
+}
